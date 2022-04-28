62c4859242552af149a78d8d0c54d26e79b4001a
oraykamberoglu <oraykamberoglu@gmail.com>
2022-04-28 11:24:53 +0300

    değişiklik yapıldı

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
@@ -8,7 +8,7 @@ def head():
 
 @app.route('/mult')
 def number():
-    var1, var2 = 3040, 7060
+    var1, var2 = 4680, 6840
     return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
 
 if __name__ == '__main__':
